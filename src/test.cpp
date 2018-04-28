46c5a012df5a616d0ee405a05a5107d8d3090522
Johannes Nixdorf <mixi@exherbo.org>
2018-04-28 11:13:22 +0200

    and even more minimal

--- src/test.cpp
+++ src/test.cpp
@@ -1,23 +1,21 @@
 #include <stdint.h>
-#include <stdlib.h>
 
 struct A {
     A() {
-        v = (uint32_t*) malloc(sizeof(uint32_t));
-        if(v)
-            *v = 123;
-    }
-    ~A() {
-        free(v);
+        v = 123;
     }
 
-    uint32_t *v;
+    ~A() {
+        v = 1;
+    }
+
+    uint32_t v;
 };
 
 A a;
 
 extern "C" {
     uint32_t get() {
-        return *a.v;
+        return a.v;
     }
 }
