186eab75a6ff093017de0c7f1279d7fafc509a47
Johannes Nixdorf <mixi@exherbo.org>
2018-04-28 10:56:26 +0200

    minimal! actually reproduce the problem in my code, not rely on the standard library to cause it

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ extern crate cc;
 fn main() {
     cc::Build::new()
         .cpp(true)
-        .cpp_link_stdlib("static=stdc++")
+        .cpp_link_stdlib(None)
         .file("src/test.cpp")
         .compile("test");
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,14 @@
-#include <cstdint>
-#include <cstdlib>
+#include <stdint.h>
+#include <stdlib.h>
 
 struct A {
     A() {
         v = (uint32_t*) malloc(sizeof(uint32_t));
         if(v)
             *v = 123;
+    }
+    ~A() {
+        free(v);
     }
 
     uint32_t *v;
